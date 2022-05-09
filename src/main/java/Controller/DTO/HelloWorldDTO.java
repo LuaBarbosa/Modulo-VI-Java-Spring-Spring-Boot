ea50f94dc535cc76e842638ebaa5e2d7a5d5081f
LuaBarbosa <lua.barbosa@yahoo.com.br>
2022-05-09 15:16:58 -0300

    Feat: criando controller e DTO

---
+++ src/main/java/Controller/DTO/HelloWorldDTO.java
@@ -0,0 +1,24 @@
+package Controller.DTO;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+    import lombok.Data;
+    import lombok.NoArgsConstructor;
+
+    import java.time.LocalDateTime;
+
+    //java bean: construtor padrao, get/sets
+    @NoArgsConstructor
+    @Data
+    public class HelloWorldDTO {
+
+        private String mensagem;
+        private String username;
+        private LocalDateTime data;
+        private String corPreferida;
+        private Integer idade;
+
+}
